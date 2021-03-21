ab80d53e71b7ee079c09a2cd1fa0e4322d50b0bc
DhruvKauts2 <kautsdhruv15@gmail.com>
2021-03-21 14:58:34 +0300

    reorganisation time

--- android/app/src/main/kotlin/com/example/u_pecpocket/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.u_pecpocket
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:_pecpocket/src/App.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(App());
-}
