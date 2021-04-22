74b068e45a07ccae103f0e151e68374b6a2fd9df
kalash25 <jainkalash25@gmail.com>
2021-04-22 20:25:25 +0530

    minor update to noti

--- backend/resources/noti_routes.py
+++ backend/resources/noti_routes.py
@@ -7,53 +7,53 @@ from datetime import datetime
 
 notiblue = Blueprint("notiblue", __name__)
 
-@notiblue.route('/noti', methods=['POST'])
-def add_noti():
-    Noti_id = request.json['Noti_id']
-    Topic = request.json['Topic']
-    Description = request.json['Description']
-    Date = request.json['Date']
-    Time = request.json['Time']
+# @notiblue.route('/noti', methods=['POST'])
+# def add_noti():
+#     Noti_id = request.json['Noti_id']
+#     Topic = request.json['Topic']
+#     Description = request.json['Description']
+#     Date = request.json['Date']
+#     Time = request.json['Time']
 
-    new_date = datetime.strptime(Date, '%d-%m-%Y').date()
-    new_time = datetime.strptime(Time, '%H:%M').time()
+#     new_date = datetime.strptime(Date, '%d-%m-%Y').date()
+#     new_time = datetime.strptime(Time, '%H:%M').time()
 
-    new_noti = Notifications(Noti_id, Topic, Description, new_date, new_time)
+#     new_noti = Notifications(Noti_id, Topic, Description, new_date, new_time)
 
-    db.session.add(new_noti)
-    db.session.commit()
+#     db.session.add(new_noti)
+#     db.session.commit()
 
-    return noti_schema.jsonify(new_noti)
-
-
-@notiblue.route('/studentnoti', methods=['POST'])
-def add_studentnoti():
-    Noti_id = request.json['Noti_id']
-    #ID = request.json['ID']
-    SID = request.json['SID']
-
-    relevant_noti = Notifications.query.filter_by(Noti_id=Noti_id).first()
-
-    if not relevant_noti :
-        # no notification for this id in notifications table
-        return jsonify({'cdoe': 505})
-
-    new_studentnoti = StudentNoti(SID, relevant_noti)
-
-    db.session.add(new_studentnoti)
-    db.session.commit()
-
-    return jsonify({'code':200})
+#     return noti_schema.jsonify(new_noti)
 
 
-@notiblue.route('/deletenotis/<Noti_id>', methods=['DELETE'])
-def delete_notis(Noti_id):
-    noti_info = Notifications.query.get(Noti_id)
+# @notiblue.route('/studentnoti', methods=['POST'])
+# def add_studentnoti():
+#     Noti_id = request.json['Noti_id']
+#     #ID = request.json['ID']
+#     SID = request.json['SID']
 
-    db.session.delete(noti_info)
-    db.session.commit()
+#     relevant_noti = Notifications.query.filter_by(Noti_id=Noti_id).first()
 
-    return jsonify({'code':200})
+#     if not relevant_noti :
+#         # no notification for this id in notifications table
+#         return jsonify({'cdoe': 505})
+
+#     new_studentnoti = StudentNoti(SID, relevant_noti)
+
+#     db.session.add(new_studentnoti)
+#     db.session.commit()
+
+#     return jsonify({'code':200})
+
+
+# @notiblue.route('/deletenotis/<Noti_id>', methods=['DELETE'])
+# def delete_notis(Noti_id):
+#     noti_info = Notifications.query.get(Noti_id)
+
+#     db.session.delete(noti_info)
+#     db.session.commit()
+
+#     return jsonify({'code':200})
 
 
 
